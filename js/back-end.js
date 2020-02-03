2d8a4b332fabd9c6af655a6f3b0563ff394fae57
pedrogonoring1 <53987520+pedrogonoring1@users.noreply.github.com>
2020-02-03 06:48:37 -0300

    Corrigido

--- js/back-end.js
+++ js/back-end.js
@@ -5,7 +5,7 @@ function escrever(btn){
         
     if (a.length == 18){
         resultados = (a[0] + a[1] + a[3] + a[4] + a[5] + a[7] + a[8] + a[9] + a[11] + a[12] + a[13] + a[14] + a[16] + a[17]);
-        document.getElementById('meuTextoVaiAqui').innerHTML = resultados;
+        document.getElementById('meuTextoVaiAqui-2').innerHTML = resultados;
         document.getElementById('link').value = resultados;
         document.getElementById("copiado").innerHTML = '✅ Copiado!';
         document.getElementById('meuTextoVaiAqui-erro').innerHTML = "";
@@ -13,11 +13,13 @@ function escrever(btn){
     }
     else if (a.length == 0){
         document.getElementById('meuTextoVaiAqui-erro').innerHTML = "😧 Você inseriu o CNPJ?";
+        document.getElementById('meuTextoVaiAqui-erro-2').innerHTML = "";
         document.getElementById('meuTextoVaiAqui').innerHTML = '';
         document.getElementById("copiado").innerHTML = '';
     }
     else if (a.length > 0 && a.length < 14){
         document.getElementById('meuTextoVaiAqui-erro-2').innerHTML = " Vish! 😥 O CNPJ está certo?";
+        document.getElementById('meuTextoVaiAqui-erro').innerHTML = "";
         document.getElementById('meuTextoVaiAqui').innerHTML = '';
         document.getElementById("copiado").innerHTML = '';
     }
