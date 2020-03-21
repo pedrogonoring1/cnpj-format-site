be9fbd472901c72d1e3d2d535ff90e48f5618077
pedrogonoring1 <53987520+pedrogonoring1@users.noreply.github.com>
2020-03-21 18:30:30 -0300

    Atualização de estilo e na melhoria da lógica
    Foi realizado uma nova melhoria em sua estilização e na lógica para execução do algorítimo de remoção dos pontos.

--- js/back-end.js
+++ js/back-end.js
@@ -7,16 +7,12 @@ function escrever(btn){
         // Pegando somente os números do CPNJ
         resultados = (a[0] + a[1] + a[3] + a[4] + a[5] + a[7] + a[8] + a[9] + a[11] + a[12] + a[13] + a[14] + a[16] + a[17]);
         //Exibindo os números sem traços no body
-        document.getElementById('meuTextoVaiAqui').innerHTML = resultados;
+        document.getElementById('meuTextoVaiAqui').innerHTML = '✅ Copiado: ' + resultados;
         // Jogando o valor para função responsável em cópiar o texto
         document.getElementById('link').value = resultados;
-        // Exibindo o status "Cópiado"
-        document.getElementById("copiado").innerHTML = '✅ Copiado!';
 
         //Limpando outros Texts
-        document.getElementById('meuTextoVaiAqui-2').innerHTML = "";
         document.getElementById('meuTextoVaiAqui-erro').innerHTML = "";
-        document.getElementById('meuTextoVaiAqui-erro-2').innerHTML = "";
 
     }
 
@@ -25,16 +21,13 @@ function escrever(btn){
         // Inserindo os pontos e traços para a variável "resultados"
         resultados = (a[0] + a[1] + '.' + a[2] + a[3] + a[4] + '.' + a[5] + a[6] + a[7] + '/' + a[8] + a[9] + a[10] + a[11] + '-' + a[12] + a[13])
         //Exibindo os números com traços no body 
-        document.getElementById('meuTextoVaiAqui-2').innerHTML = resultados;
+        document.getElementById('meuTextoVaiAqui').innerHTML = '✅ Copiado: ' + resultados;
         // Jogando o valor para função responsável em cópiar o texto
         document.getElementById('link').value = resultados;
         // Exibindo o status "Cópiado"
-        document.getElementById("copiado").innerHTML = '✅ Copiado!';
 
         //Limpando outros Texts
-        document.getElementById('meuTextoVaiAqui').innerHTML = "";
         document.getElementById('meuTextoVaiAqui-erro').innerHTML = "";
-        document.getElementById('meuTextoVaiAqui-erro-2').innerHTML = "";
     }
 
 
@@ -42,23 +35,28 @@ function escrever(btn){
 
     // Se nada estiver digitado:
     else if (a.length == 0){
-        document.getElementById('meuTextoVaiAqui-erro').innerHTML = "😧 Você não inseriu o CNPJ.";
+        document.getElementById('meuTextoVaiAqui-erro').innerHTML = "😕 Você não inseriu o CNPJ";
 
         //Limpar outros texts
-        document.getElementById('meuTextoVaiAqui-erro-2').innerHTML = "";
         document.getElementById('meuTextoVaiAqui').innerHTML = "";
-        document.getElementById('meuTextoVaiAqui-2').innerHTML = "";
         document.getElementById("copiado").innerHTML = '';
     }
     
     // Se o texto digitado estiver errado, faltando caracteres:
-    else if (a.length > 0 && a.length < 14){
-        document.getElementById('meuTextoVaiAqui-erro-2').innerHTML = " Vish! 😥 O CNPJ está certo?";
+    else if (a.length > 18 || a.length < 14){
+        document.getElementById('meuTextoVaiAqui-erro').innerHTML = "😭 Ops! CNPJ digitado errado";
 
         //Limpar outros texts
-        document.getElementById('meuTextoVaiAqui-erro').innerHTML = "";
         document.getElementById('meuTextoVaiAqui').innerHTML = "";
-        document.getElementById('meuTextoVaiAqui-2').innerHTML = "";
+        document.getElementById("copiado").innerHTML = '';
+    }
+    
+    // Se o texto digitado estiver errado, faltando caracteres:
+    else if(a.length > 14 && a.length < 18){
+        document.getElementById('meuTextoVaiAqui-erro').innerHTML = "😭 Ops! CNPJ digitado errado";
+
+        //Limpar outros texts
+        document.getElementById('meuTextoVaiAqui').innerHTML = "";
         document.getElementById("copiado").innerHTML = '';
     }
     
@@ -67,9 +65,9 @@ function escrever(btn){
 // função copiar texto:
 function copiarTexto(){
     // Coletando o CNPJ
-    var textoCopiado = document.getElementById("link");
+    var textoCopiado = document.getElementById("link").value;
     // Selecionaod o CNPJ
-    textoCopiado.select();
+    document.getElementById("link").select();
     // Executando a function para cópiar o CNPJ
     document.execCommand("Copy");
 }
